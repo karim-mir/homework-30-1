3af485c7b6685ef79bf9bead544874478386592b
NazKar@Nazir <karimov.nazir00@yandex.ru>
2025-06-26 18:11:08 +0500

    Инициализация приложения educations

---
+++ educations/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+class Course(models.Model):
+    course_name = models.CharField(
+        max_length=35,
+        verbose_name="Название курса",
+        blank=True,
+        help_text="Введите название курса",
+    )
+    preview = models.ImageField(
+        upload_to="educations/previews/",
+        verbose_name="Превью курса",
+        blank=True,
+        help_text="Загрузите превью курса",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание курса",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return f"Название курса: {self.course_name}"
+
+
+class Lesson(models.Model):
+    lesson_name = models.CharField(
+        max_length=35,
+        verbose_name="Название урока",
+        blank=True,
+        help_text="Введите название урока",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание курса",
+    )
+    preview = models.ImageField(
+        upload_to="educations/previews/",
+        verbose_name="Превью урока",
+        blank=True,
+        help_text="Загрузите превью урока",
+    )
+    video_url = models.URLField(
+        verbose_name="Ссылка на видео",
+        blank=True,
+        help_text="Введите URL видео урока",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        verbose_name="Курс",
+        help_text="Курс, к которому относится урок",
+    )
+
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return (f"Название урока: {self.lesson_name}"
+                f"Описание урока: {self.description}")
---
+++ educations/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, generics
+
+from educations.models import Course, Lesson
+from educations.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
